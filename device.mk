d95faab3fffc3920373c7cc4df87ac478da4ef49
Alastair Donaldson <afdx@google.com>
2020-02-07 10:31:14 +0000

    griffin: Add android.software.vulkan.deqp.level, version 2019-03-01
    This change sets the new Vulkan dEQP level feature flag to the value
    corresponding to the Vulkan dEQP tests for Android 10.
    
    Change-Id: I0ad13a42a5d47a8a493adc689222dfbb495f9850

--- device.mk
+++ device.mk
@@ -51,7 +51,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2019-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Properties
 -include $(LOCAL_PATH)/properties.mk
